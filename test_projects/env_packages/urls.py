1d1f9a6bd6d9110d1b5d039a24f48abbe0cf5198
vsirisanthana <vsirisanthana@gmail.com>
2012-02-15 20:38:22 +0000

    Remove product app Restructure tests

--- test_projects/env_packages/urls.py
+++ test_projects/env_packages/urls.py
@@ -1,19 +1,17 @@
 from django.conf.urls.defaults import patterns, include, url
 
 # Uncomment the next two lines to enable the admin:
-from django.contrib import admin
-admin.autodiscover()
+# from django.contrib import admin
+# admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'adultelicious.views.home', name='home'),
-    # url(r'^adultelicious/', include('adultelicious.foo.urls')),
+    # url(r'^$', 'env_packages.views.home', name='home'),
+    # url(r'^env_packages/', include('env_packages.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    url(r'^admin/', include(admin.site.urls)),
-
-    url(r'^', include('product.urls')),
+    # url(r'^admin/', include(admin.site.urls)),
 )
